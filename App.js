8d83403f7e7c1126d281b3a80c45872cbc1c2f26
Aman <aakashaman1770@gmail.com>
2021-10-23 00:42:58 +0530

    Added PR

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+export default function App() {
+  return <WelcomeScreen />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "orange",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
